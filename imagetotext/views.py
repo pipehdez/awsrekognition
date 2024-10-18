5977eade0bbc613550098a9108989430c778e3f1
andreshernandez-lafise <151097543+andreshernandez-lafise@users.noreply.github.com>
2024-10-17 23:28:58 -0500

    prompt 3.1

--- imagetotext/views.py
+++ imagetotext/views.py
@@ -53,7 +53,7 @@ class OcrAwsViewSet(viewsets.ModelViewSet):
 				#os.environ['PROMPT']
 
 				# ollama respose
-				response = client.chat(model='llama3.2:1b', messages=[
+				response = client.chat(model='llama3.1', messages=[
 					{
 						'role': 'user',
 						'content': f"{prompt}:\n{textDetections['text']}",
