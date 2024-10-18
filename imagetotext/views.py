d22555ad45456cfa023c5d9acf347ae07a0221f7
andreshernandez-lafise <151097543+andreshernandez-lafise@users.noreply.github.com>
2024-10-18 00:08:42 -0500

    nuevo prompt

--- imagetotext/views.py
+++ imagetotext/views.py
@@ -45,10 +45,11 @@ class OcrAwsViewSet(viewsets.ModelViewSet):
 				# prompt = """
 				# Analiza el siguiente texto y extrae todas las preguntas y datos de formulario en una estructura JSON. El formato debe ser un array de objetos con la siguiente estructura: [{ "question": "", "type": "", "options": [{ "label": "", "value": "" }] }]. Si se menciona o es necesario incluir firmas o fechas, añádelas; de lo contrario, no las incluyas. No devuelvas ningún texto adicional, solo el JSON. Si no puedes identificar el tipo de input, usa 'text' como valor predeterminado. Asegúrate de que el JSON sea válido y siga estrictamente el formato especificado
 				# """
+
+				# Analiza el siguiente texto: saca la comprobación de titulo como el procedureName y saca los conceptos: revisando conforme y datos de formulario en una misma estructura y devuelvelo en una estructura json y en el idioma """ + languaje + """, ejemplo de como debe devolver la estructura: [{ procedureName: 'string', tasks: [{ "taskName": 'string', "done": boolean }]}] , importante: No incluyas ningún texto adicional en tu respuesta, Solo el JSON, Asegúrate de que el JSON sea válido y siga exactamente la estructura proporcionada:
 				languaje = image = request.data['languaje']
 				prompt = """
-					Analiza el siguiente texto: saca la comprobación de titulo como el procedureName y saca los conceptos: revisando conforme y datos de formulario en una misma estructura y devuelvelo en una estructura json y en el idioma """ + languaje + """, ejemplo de como debe devolver la estructura: [{ procedureName: 'string', tasks: [{ "taskName": 'string', "done": boolean }]}] , importante: No incluyas ningún texto adicional en tu respuesta, Solo el JSON, Asegúrate de que el JSON sea válido y siga exactamente la estructura proporcionada:
-				"""
+					Analiza el siguiente texto: la informacion del este formulario como un objeto json, lo mas importante la lista de tarea en elcuadro con respuesta de si/no en este formato: tasks: [{ "taskName": 'string', "done": boolean }] y el nombre de la comprobacion como procedureName: 'string' el objeto al final debe quedar asi el ejemplo: [{ procedureName: 'string', tasks: [{ "taskName": 'string', "done": boolean }]}] importante: No incluyas ningún texto adicional en tu respuesta, Solo el JSON, Asegúrate de que el JSON sea válido y siga exactamente la estructura proporcionada, y por ultimo el idioma debe ser """ + languaje
 				
 				#os.environ['PROMPT']
 
